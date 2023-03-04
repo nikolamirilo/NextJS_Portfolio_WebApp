dcd560ad3fc025f3a6b56b596c06dfb4453c1e87
Nikola Mirilo <nikolamirilo@gmail.com>
2023-03-04 17:53:40 +0100

    changed env variable3s

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
   },
   env: {
     MONGODB_URI: "mongodb+srv://nikolamirilo:veslanje123@cluster0.6kvwzdu.mongodb.net/?retryWrites=true&w=majority",
-    WEB_APP_URL: process.env.NODE_ENV === "production" ? "https://mirilo-nikola.netlify.app" : "http://localhost:3000",
+    WEB_APP_URL: "https://mirilo-nikola.netlify.app",
   },
   images: {
     remotePatterns: [
