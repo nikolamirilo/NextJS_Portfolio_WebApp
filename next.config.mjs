b5fb772a6d1e9228cb2502330c48b85a09150a80
nikolamirilo <nikolamirilo@gmail.com>
2023-03-06 22:38:47 +0100

    conditional environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,10 @@ const nextConfig = {
   env: {
     MONGODB_URI:
       "mongodb+srv://nikolamirilo:veslanje123@cluster0.6kvwzdu.mongodb.net/?retryWrites=true&w=majority",
-    WEB_APP_URL: "https://mirilo-nikola.netlify.app",
+    WEB_APP_URL:
+      process.env.ENVIRONMENT === "PRODUCTION"
+        ? "https://mirilo-nikola.netlify.app/"
+        : "http://localhost:3000",
   },
   images: {
     remotePatterns: [
--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -19,10 +19,10 @@ import React, { Suspense, useEffect, useState } from "react";
 export const getStaticPaths = async () => {
   return {
     paths: [
-      // { params: { id: "1" } },
-      // { params: { id: "2" } },
-      // { params: { id: "3" } },
-      // { params: { id: "4" } },
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+      { params: { id: "4" } },
     ], //indicates that no page needs be created at build time
     fallback: "blocking", //indicates the type of fallback
   };
@@ -31,12 +31,13 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context: any) => {
   const id = context.params.id;
   const response = await fetch(`${process.env.WEB_APP_URL}/api/projects/${id}`);
+
   const data = await response.json();
   return {
     props: {
       project: data.singleProject ? data.singleProject : null,
     },
-    // revalidate: 10,
+    revalidate: 10,
   };
 };
 
