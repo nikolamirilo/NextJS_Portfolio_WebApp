d074db3c0c642ed3daab1704fc1ca8b6671ae2cc
nikolamirilo <nikolamirilo@gmail.com>
2023-03-10 11:29:33 +0100

    revert ISM

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,47 +1,46 @@
 import { Loader } from "@/components";
 import { SingleProjectProps } from "@/typescript/interfaces/pages";
-import { GetServerSidePropsContext } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import React, { Suspense, useEffect, useState } from "react";
 
 //!SERVER-SIDE RENDERING
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const id = context.params?.id;
-  const response = await fetch(`${process.env.WEB_APP_URL}/api/projects/${id}`);
-  const data = await response.json();
-  context.res.setHeader("Cache-Control", "public, s-maxage=10, stale-while-revalidate=59");
-  return {
-    props: {
-      project: data.singleProject ? data.singleProject : null,
-    },
-  };
-}
-
-//!INCREMENTAL STATIC REGENERATION
-// export const getStaticPaths = async () => {
-//   const response = await fetch(`${process.env.WEB_APP_URL}/api/projects`);
-//   const data = await response.json();
-//   // Get the paths we want to pre-render based on posts
-//   const allProjects: Project[] = data.allProjects;
-//   const paths = allProjects.map((item: Project) => ({
-//     params: { id: item.id },
-//   }));
-//   return { paths, fallback: "blocking" };
-// };
-
-// export const getStaticProps = async (context: any) => {
-//   const id = context.params.id;
+// export async function getServerSideProps(context: GetServerSidePropsContext) {
+//   const id = context.params?.id;
 //   const response = await fetch(`${process.env.WEB_APP_URL}/api/projects/${id}`);
 //   const data = await response.json();
+//   context.res.setHeader("Cache-Control", "public, s-maxage=10, stale-while-revalidate=59");
 //   return {
 //     props: {
 //       project: data.singleProject ? data.singleProject : null,
 //     },
-//     revalidate: 60,
 //   };
-// };
+// }
+
+//!INCREMENTAL STATIC REGENERATION
+export const getStaticPaths = async () => {
+  const response = await fetch(`${process.env.WEB_APP_URL}/api/projects`);
+  const data = await response.json();
+  // Get the paths we want to pre-render based on posts
+  const allProjects: Project[] = data.allProjects;
+  const paths = allProjects.map((item: Project) => ({
+    params: { id: item.id },
+  }));
+  return { paths, fallback: "blocking" };
+};
+
+export const getStaticProps = async (context: any) => {
+  const id = context.params.id;
+  const response = await fetch(`${process.env.WEB_APP_URL}/api/projects/${id}`);
+  const data = await response.json();
+  return {
+    props: {
+      project: data.singleProject ? data.singleProject : null,
+    },
+    revalidate: 60,
+  };
+};
 
 const SingleProject: React.FC<SingleProjectProps> = ({ project }) => {
   const [loading, setLoading] = useState(true);
