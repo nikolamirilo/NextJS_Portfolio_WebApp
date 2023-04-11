2ca22bc34b007308e5059fa2603eef4fe73e4614
nikolamirilo <nikolamirilo@gmail.com>
2023-04-11 14:14:32 +0200

    fixed bugs on hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,12 @@
 import { HeroProps } from "@/typescript/interfaces/sections";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import Image from "next/image";
 
 const Hero: React.FC<HeroProps> = ({ title, subtitle, image }) => {
-  const blurDataURL =
-    "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMTI4IDEyOCIgd2lkdGg9IjEyOCIgaGVpZ2h0PSIxMjgiIHZlcnNpb249IjEuMSI+PHBhdGggZD0iTTAgMGgxMjh2MTI4SDB6IiBmaWxsPSIjMDAxZjNmIiAvPjwvc3ZnPg==";
   return (
     <section id="hero">
-      <Image src={image} alt="Office" fill placeholder="blur" blurDataURL={blurDataURL} priority />
+      <Image src={image} alt="Office" fill object-fit="cover" priority />
       <div className="hero-content">
         <h1>{title}</h1>
         <h2>{subtitle}</h2>
