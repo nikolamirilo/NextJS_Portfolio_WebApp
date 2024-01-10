99d6b2b387eb3212f7b42ca10716934a842bf648
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2024-01-10 13:24:40 +0100

    added navbar closing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className="navbar">
       <div className="logo">
@@ -14,17 +19,34 @@ const Navbar: React.FC = () => {
           style={{ margin: "1vh 0" }}
         />
       </div>
-      <input id="menu-toggle" type="checkbox" />
+      <input
+        id="menu-toggle"
+        type="checkbox"
+        checked={isOpen}
+        onChange={handleNavbar}
+      />
       <label className="menu-button-container" htmlFor="menu-toggle">
         <div className="menu-button"></div>
       </label>
       <div className="links">
-        <Link href="/">Home</Link>
-        <Link href="#about">About me</Link>
-        <Link href="#services">Services</Link>
-        <Link href="#portfolio">Portfolio</Link>
-        <Link href="#education">Education</Link>
-        <Link href="mailto:nikolamirilo.com">Contact me</Link>
+        <Link href="#hero" onClick={handleNavbar}>
+          Home
+        </Link>
+        <Link onClick={handleNavbar} href="#about">
+          About me
+        </Link>
+        <Link onClick={handleNavbar} href="#services">
+          Services
+        </Link>
+        <Link onClick={handleNavbar} href="#portfolio">
+          Portfolio
+        </Link>
+        <Link onClick={handleNavbar} href="#education">
+          Education
+        </Link>
+        <Link onClick={handleNavbar} href="mailto:nikolamirilo.com">
+          Contact me
+        </Link>
       </div>
     </nav>
   );
