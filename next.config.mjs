5a43c32b94cb5bca54e5593a8413c56a03670e62
Nikola.Mirilo <nikola.mirilo@adacta-fintech.com>
2024-01-11 10:09:33 +0100

    changed portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,11 @@ const nextConfig = {
       { hostname: "images.credly.com" },
       { hostname: "th.bing.com" },
       { hostname: "www.lexpressproperty.com" },
+      { hostname: "beautybyclaire.rs" },
+      { hostname: "instagram.fbeg2-1.fna.fbcdn.net" },
+      { hostname: "www.ultra-tim-s.com" },
+      { hostname: "is4-ssl.mzstatic.com" },
+      { hostname: "webuyindubai.com" },
     ],
   },
 };
